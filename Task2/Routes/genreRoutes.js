3ff6bccd6d6167962a3f4e26d0ff8db8d1778620
Dzianis Kundaliou <dark_dionis@mail.ru>
2023-03-11 12:21:03 +0300

    First version of realisation has been deleted

--- Task2/Routes/genreRoutes.js
+++
@@ -1,8 +0,0 @@
-const Router = require('express');
-const router = new Router();
-const ganreController = require('../Controllers/ganreController');
-
-router.get('/genres', ganreController.getGanres);
-router.get('/genres', ganreController.getFilmsWithGanre);
-
-module.exports = router;
